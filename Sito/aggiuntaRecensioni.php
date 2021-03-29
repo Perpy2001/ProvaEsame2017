7aeb8de53f2b146b896f4c667a92d295d8dd2000
denisbellin <45967516+denisbellin@users.noreply.github.com>
2021-03-29 16:15:33 +0200

    aggiuntaRecensioni

--- Sito/aggiuntaRecensioni.php
+++ Sito/aggiuntaRecensioni.php
@@ -9,4 +9,17 @@ if (isset($_POST['voto'])) {
 if (isset($_POST['note'])) {
     $note= $_POST['note'];
 }
+$mysqli = new mysqli('localhost', 'root', '', 'prova_esame_2017');
+    if ($mysqli->connect_error) {
+        die('Errore di connessione (' . $mysqli->connect_errno . ')' . $mysqli->connect_error);
+    } else {
+        $trovaCF =  "SELECT CF FROM utente WHERE email = '$email'";
+        $ris = $mysqli ->query($trovaCF);
+        if (mysqli_num_rows($ris)!=0) {
+            while($riga = $ris ->fetch_array(MYSQLI_ASSOC)){
+                $inserimento = "INSERT INTO `recensioni`(`CodRec`, `numerico`, `note`, `tipo`, `CF`) VALUES ([value-1],[value-2],[value-3],[value-4],[value-5])";
+                $mysqli->query($inserimento);
+            }
+            } else header("Location: recensioni.php?errore='Mail sbagliata'");
+    }
 ?>
